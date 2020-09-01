cc1c68d05d87d85a3fe124f3026cf5a2e4712028
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-08-31 20:18:08 -0400

    Created theme configuration style

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "05d2329ea574efc2afa8";
+/******/ 	var hotCurrentHash = "49db670817e330693b79";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
