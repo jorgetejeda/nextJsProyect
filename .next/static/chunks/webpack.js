37196768877dc5b01e9e407fc840768aa1ba00ae
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-08-31 21:35:58 -0400

    it was created an object button which will be contain information about menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ea90aeff08f3c21a1b76";
+/******/ 	var hotCurrentHash = "0888eac01dfe6298f980";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
